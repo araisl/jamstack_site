f0b50a2d9b7870e86ba5c67168144bf1941fa085
araisl <raisl@taktsoft.com>
2021-09-15 12:35:15 +0200

    Initial Commit

---
+++ src/js/vibe.js
@@ -0,0 +1,10 @@
+const vibes = [
+  "...and you are awesome!",
+  "...have a wonderful day!",
+  "...and you 've got this!",
+  "...and you are a puppy"
+];
+
+let vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+document.querySelector(".vibe").append(vibe);
